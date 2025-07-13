2ad69e942771d55a790d9c36478921d02f825ffb
AngelinaVB <b.lina.1995@mail.ru>
2025-07-13 23:14:12 +0300

    Init commit

---
+++ src/js/app.js
@@ -0,0 +1,14 @@
+// TODO: write your code here
+
+const obj = {name: 'мечник', health: 10, level: 2, attack: 80, defence: 40};
+
+["name", "level"];
+
+orderByProps(obj, ["name", "level"]);
+
+
+import sum from './basic';
+
+console.log('worked');
+
+console.log(sum([1, 2]));
