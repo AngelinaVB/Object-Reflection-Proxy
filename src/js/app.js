92571bf18e696f35cb1a51f03cd440b7a7156274
AngelinaVB <b.lina.1995@mail.ru>
2025-07-25 21:39:19 +0300

    задание частично готово

--- src/js/app.js
+++ src/js/app.js
@@ -2,10 +2,26 @@
 
 const obj = {name: 'мечник', health: 10, level: 2, attack: 80, defence: 40};
 
-["name", "level"];
+export default function orderByProps(obj, [name, level]) {
+  const result = [];
+  const keys = Object.keys(obj).sort(); 
 
-orderByProps(obj, ["name", "level"]);
+  for (let i = 0; i < keys.length; i += 1) { 
+    if (keys[i] === name) {
+      result.unshift({ key: keys[i], value: obj[keys[i]] });
+    } if (keys[i] === level) {
+      result.push({ key: keys[i], value: obj[keys[i]] });
+    }
+  }
 
+  for (const i in keys) { 
+    if (keys[i] !== name && keys[i] !== level) {
+      result.push({ key: keys[i], value: obj[keys[i]]});
+    }
+  }
+
+  return result;
+}
 
 import sum from './basic';
 
