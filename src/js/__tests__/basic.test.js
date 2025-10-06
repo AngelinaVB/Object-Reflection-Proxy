7dd1c7487752a2833d35160902ff6f9723dbb20b
AngelinaVB <b.lina.1995@mail.ru>
2025-10-06 22:50:28 +0300

    первое задание частично готово

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -1,7 +1,13 @@
-import sum from '../basic';
+import {orderByProps} from '.app.js';
 
-test('should sum', () => {
-  const result = sum([1, 2, 3]);
+test('should arrSort', () => {
+  const result = [
+  {key: "name", value: "мечник"}, // порядок взят из массива с ключами
+  {key: "level", value: 2}, // порядок взят из массива с ключами
+  {key: "attack", value: 80}, // порядок по алфавиту (т.к. в массиве с ключами нет значения "attack")
+  {key: "defence", value: 40}, // порядок по алфавиту (т.к. в массиве с ключами нет значения "defence")
+  {key: "health", value: 10} // порядок по алфавиту (т.к. в массиве с ключами нет значения "health")
+];
 
-  expect(result).toBe(6);
+  expect(result).toBe(orderByProps);
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,30 +1,22 @@
 // TODO: write your code here
-
-const obj = {name: 'мечник', health: 10, level: 2, attack: 80, defence: 40};
-
 export default function orderByProps(obj, [name, level]) {
-  const result = [];
+  const obj = {name: 'мечник', health: 10, level: 2, attack: 80, defence: 40};
+  const arrSort = [];
   const keys = Object.keys(obj).sort(); 
 
   for (let i = 0; i < keys.length; i += 1) { 
     if (keys[i] === name) {
-      result.unshift({ key: keys[i], value: obj[keys[i]] });
+      arrSort.unshift({ key: keys[i], value: obj[keys[i]] });
     } if (keys[i] === level) {
-      result.push({ key: keys[i], value: obj[keys[i]] });
+      arrSort.push({ key: keys[i], value: obj[keys[i]] });
     }
   }
 
   for (const i in keys) { 
     if (keys[i] !== name && keys[i] !== level) {
-      result.push({ key: keys[i], value: obj[keys[i]]});
+      arrSort.push({ key: keys[i], value: obj[keys[i]]});
     }
   }
 
-  return result;
+  return arrSort;
 }
-
-import sum from './basic';
-
-console.log('worked');
-
-console.log(sum([1, 2]));
